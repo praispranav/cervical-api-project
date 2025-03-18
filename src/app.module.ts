51c9b052c4ae68e6ae32c5201053b9378d5729b4
Amit <amit.sharma@fairtekconsulting.com>
2025-03-19 01:15:26 +0530

    Added Proxy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        createProxyMiddleware({
+          target: 'http://localhost:5005', // Replace with your Express server's address
+          changeOrigin: true, // For virtual hosted sites
+          pathRewrite: {
+            '^/express-api': '', // Remove the prefix from the request
+          },
+        }),
+      )
+      .forRoutes('/express-api/*'); // Route prefix that triggers proxy
+  }
+}
