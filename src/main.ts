f67355346b17d10efbed63cc1c50eadee5da7cfd
Pranav Kumar <praispranav@gmail.com>
2025-03-19 05:26:04 +0000

    Added Mobile Code

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+require('dotenv').config(); 
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 8909);
+}
+bootstrap();
