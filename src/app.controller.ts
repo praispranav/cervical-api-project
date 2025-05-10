071f7b40c750386ebd17aa64c31998d33874d346
Amit <amit.sharma@fairtekconsulting.com>
2025-05-10 22:40:50 +0530

    Added Mothers Day Template

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -57,7 +57,7 @@ export class AppController {
     doc.pipe(res);
 
     try {
-      doc.image(path.join(__dirname, '..', 'certificate.jpeg'), 0, 0, {
+      doc.image(path.join(__dirname, '..', 'certificate_cervical.jpeg'), 0, 0, {
         width: 842,
         height: 595,
       });
@@ -129,7 +129,7 @@ export class AppController {
     const stream = fs.createWriteStream(outputFilePath);
 
     doc.pipe(stream);
-    doc.image(path.join(process.cwd(), 'certificate.jpeg'), 0, 0, {
+    doc.image(path.join(process.cwd(), 'certificate_cervical.jpeg'), 0, 0, {
       width: 842,
       height: 595,
     });
@@ -162,6 +162,167 @@ export class AppController {
         .fontSize(15)
         .fillColor('black')
         .text(city + ', ' + a, 80, 468, { align: 'center' });
+
+    if (additionalText) {
+      doc
+        .fontSize(18)
+        .fillColor('red')
+        .text(additionalText, 80, 295, { align: 'center' });
+    }
+    doc.end();
+
+    stream.on('finish', () => {
+      res.download(outputFilePath, `certificate_${name}.pdf`, (err) => {
+        if (err) {
+          console.error('Download error:', err);
+          return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send('Error downloading file');
+        }
+        fs.unlinkSync(outputFilePath);
+      });
+    });
+  }
+
+  @Get('generate-certificate-mothers-day')
+  @Header('Content-Type', 'application/pdf')
+  @Header('Content-Disposition', 'inline; filename=certificate.pdf')
+  async generateCertificateMothersDay(
+    @Res() res: Response,
+    @Query('name') name: string = '',
+    @Query('email') email: string = '',
+    @Query('phone') phone: string = '',
+    @Query('city') city: string = '',
+    @Query('additionalText') additionalText: string = '',
+  ) {
+    if (!name) {
+      throw new HttpException('Name is required', HttpStatus.BAD_REQUEST);
+    }
+    try {
+      this.appService.createCertificate({ deviceType: DeviceType.Desktop, name, email, phone, city, timestamp: new Date() });
+    } catch (error) {}
+
+    function formatName(name) {
+      return name
+        .toLowerCase() // Convert everything to lowercase
+        .split(' ') // Split by space
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize first letter
+        .join(' '); // Join words back together
+    }
+
+    name = formatName(name);
+
+    const doc = new PDFDocument({
+      size: 'A4',
+      layout: 'landscape',
+    });
+
+    doc.pipe(res);
+
+    try {
+      doc.image(path.join(__dirname, '..', 'mother_day_certificate.jpg'), 0, 0, {
+        width: 842,
+        height: 595,
+      });
+
+      const fontBuffer = fs.readFileSync(
+        path.join(__dirname, '..', 'Boldonse-Regular.ttf'),
+      );
+      doc
+        .font(fontBuffer)
+        .fontSize(25)
+        .fillColor('black')
+        .text(name, 80, 335, { align: 'center' });
+
+      const fontBuffer2 = fs.readFileSync(
+        path.join(__dirname, '..', 'Montserrat-SemiBold.ttf'),
+      );
+      const a = moment(new Date()).format('MMMM Do, YYYY');
+      doc
+        .font(fontBuffer2)
+        .fontSize(15)
+        .fillColor('black')
+        .text(city + ', ' + a, 80, 458, { align: 'center' });
+
+      if (additionalText) {
+        doc
+          .fontSize(18)
+          .fillColor('red')
+          .text(additionalText, 80, 295, { align: 'center' });
+      }
+
+      doc.end();
+    } catch (error) {
+      console.error('Error generating PDF:', error);
+      throw new HttpException(
+        'Failed to generate PDF',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  @Post('generate-certificate-mothers-day-mobile')
+  async generateCertificateMothersDayMobile(
+    @Query('name') name: string,
+    @Query('additionalText') additionalText: string,
+    @Query('email') email: string,
+    @Query('city') city: string,
+    @Query('phone') phone: string,
+    @Res() res: Response,
+  ) {
+    if (!name) {
+      return res.status(HttpStatus.BAD_REQUEST).json({ error: 'Name is required' });
+    }
+
+    try {
+      this.appService.createCertificate({ deviceType: DeviceType.Mobile, name, email, phone, city, timestamp: new Date() });
+    } catch (error) {}
+
+    try {
+      fs.writeFileSync(
+        path.join(process.cwd(), 'certificates', `${v4()}.json`),
+        JSON.stringify({ name, email, phone, city }),
+      );
+    } catch (error) {
+      console.error('Error writing JSON:', error);
+    }
+
+    const doc = new PDFDocument({ size: 'A4', layout: 'landscape' });
+    const outputFilePath = path.join(process.cwd(), `certificate_${name}.pdf`);
+    const stream = fs.createWriteStream(outputFilePath);
+
+    doc.pipe(stream);
+    doc.image(path.join(process.cwd(), 'mother_day_certificate.jpg'), 0, 0, {
+      width: 842,
+      height: 595,
+    });
+
+    function formatName(inputName: string): string {
+      return inputName
+        .toLowerCase()
+        .split(' ')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+    }
+
+    const formattedName = formatName(name);
+
+    const fontBuffer = fs.readFileSync(
+      path.join(__dirname, '..', 'Boldonse-Regular.ttf'),
+    );
+    doc
+      .font(fontBuffer)
+      .fontSize(25)
+      .fillColor('black')
+      .text(formattedName, 80, 335, { align: 'center' });
+
+      const fontBuffer2 = fs.readFileSync(
+        path.join(__dirname, '..', 'Montserrat-SemiBold.ttf'),
+      );
+      const a = moment(new Date()).format('MMMM Do, YYYY');
+      doc
+        .font(fontBuffer2)
+        .fontSize(15)
+        .fillColor('black')
+        .text(city + ', ' + a, 80, 458, { align: 'center' });
 
     if (additionalText) {
       doc
